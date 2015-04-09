9bdbc1ed4a037ad05be783689b866b2ecc9801d8
GilesMetcalf <giles@lughnasadh.com>
2015-04-09 09:11:31 +0100

    Update disable.php

--- disable.php
+++ disable.php
@@ -1,4 +1,17 @@
 <?php
+/**
+* This file is part of the "MensaCymru Calendar" plugin for Wolf CMS.
+* Licensed under an GPL style license. For full details see license.txt.
+*
+* @author Giles Metcalf <giles@lughnasadh.com>
+* @copyright Giles Metcalf, 2015
+*
+* Original authors:
+*
+* @author Jacek Ciach <jacek.ciach@wp.eu>
+* @copyright Jacek Ciach, 2014
+*
+*/
 
 if (!defined('IN_CMS')) { exit(); }
 
