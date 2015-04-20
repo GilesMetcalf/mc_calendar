dea36485068420b29760f1d7595167d4abfbdd29
GilesMetcalf <giles@lughnasadh.com>
2015-04-20 12:47:31 +0100

    Update event_frontend.php

--- views/event_frontend.php
+++ views/event_frontend.php
@@ -13,15 +13,25 @@
 *
 */
 
+/* Old code:
+* 
+* <div class="event" id="event<?=$id?>">
+* <h3><?=$date_from?><?php if (!empty($date_to)) echo " ".__("to")." $date_to"; ?>: <?=$title?></h3>
+* <p><?=$content?></p>
+* <?php if ($show_author): ?>
+* <p class="info"><?php echo __("Posted by").": $author"; ?></p>
+* <?php endif; ?>
+* </div> 
+* 
+*/
+
 if (!defined('IN_CMS')) { exit(); }
 
 /* Format and display data */
 ?>
   
-<div class="event" id="event<?=$id?>">
-<h3><?=$date_from?><?php if (!empty($date_to)) echo " ".__("to")." $date_to"; ?>: <?=$title?></h3>
-<p><?=$content?></p>
-<?php if ($show_author): ?>
-<p class="info"><?php echo __("Posted by").": $author"; ?></p>
-<?php endif; ?>  
+<div class="event" id="event<?=$event->getId();?>">
+<h3><?=$event->getDateFrom();?><?php if (!empty($event->getDateTo())) echo " ".__("to")." $event->getDateTo()"; ?>: <?=$event->getTitle();?></h3>
+<p><?=$event->getDescription();?></p>
+
 </div>    
